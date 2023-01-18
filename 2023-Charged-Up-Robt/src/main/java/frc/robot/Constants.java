1dee0e4b41357bb43f45c28e78852d366981ac45
Team 6749 <ERARobotics6749@gmail.com>
2023-01-17 18:03:42 -0600

    copied working swerve code and integrated april tag recognition
    swerve module class and swerve drivebase class, as well as constants

--- 2023-Charged-Up-Robt/src/main/java/frc/robot/Constants.java
+++ 2023-Charged-Up-Robt/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import frc.robot.subsystems.SwerveDriveModule;
+import frc.robot.subsystems.SwerveDriveSubsystem;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +18,13 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+  public static class DrivebaseConstants {
+    //subsystem constructed with array of modules
+    public static SwerveDriveModule frontLeftModule = new SwerveDriveModule("frontLeftModule", 0, 0, 0, 0, null);
+    public static SwerveDriveModule frontRightModule = new SwerveDriveModule("frontRightModule", 0, 0, 0, 0, null);
+    public static SwerveDriveModule backLeftModule = new SwerveDriveModule("backLeftModule", 0, 0, 0, 0, null);
+    public static SwerveDriveModule backRightModule = new SwerveDriveModule("backRightModule", 0, 0, 0, 0, null);
+    public static SwerveDriveSubsystem swerveDriveSubsystem = new SwerveDriveSubsystem(new SwerveDriveModule [] {frontLeftModule, frontRightModule, backLeftModule, frontRightModule});
   }
 }
