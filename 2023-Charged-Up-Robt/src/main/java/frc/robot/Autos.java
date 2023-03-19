92336adae92f92a06d3e7a38869785f5d0818cbb
Team 6749 <35408444+6749computers@users.noreply.github.com>
2023-03-19 17:49:05 -0500

    Update Autos.java

--- 2023-Charged-Up-Robt/src/main/java/frc/robot/Autos.java
+++ 2023-Charged-Up-Robt/src/main/java/frc/robot/Autos.java
@@ -22,7 +22,6 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
