42516d7c4b085d4fdc83d1f05c43c702daf07d1d
Team 6749 <35408444+6749computers@users.noreply.github.com>
2023-03-19 17:43:32 -0500

    removed unused imports

--- 2023-Charged-Up-Robt/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Charged-Up-Robt/src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import frc.robot.subsystems.SwerveDriveSubsystem;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
--- 2023-Charged-Up-Robt/src/main/java/frc/robot/commands/SwerveDriveWithController.java
+++ 2023-Charged-Up-Robt/src/main/java/frc/robot/commands/SwerveDriveWithController.java
@@ -5,11 +5,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
