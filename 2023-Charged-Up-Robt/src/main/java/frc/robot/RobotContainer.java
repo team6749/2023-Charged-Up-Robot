6c4fabca513d18278a942a03776060df8e3e830d
ERA Robotics <35408444+ERArobotics@users.noreply.github.com>
2023-03-22 16:58:28 -0500

    removed unused impoirts

--- 2023-Charged-Up-Robt/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Charged-Up-Robt/src/main/java/frc/robot/RobotContainer.java
@@ -4,28 +4,24 @@
 
 package frc.robot;
 
-import frc.robot.commands.ClawControl;
 import frc.robot.commands.ClawToggle;
 import frc.robot.commands.DoAutoAlignmentAndScore;
 import frc.robot.commands.DriveToSubstation;
 import frc.robot.commands.LineUpWithStation;
 import frc.robot.commands.SelfBalance;
 import frc.robot.commands.SwerveDriveWithController;
-import frc.robot.enums.ScoringType;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.SwerveDriveSubsystem;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.ClawSubsystem;
 import frc.robot.commands.MoveArmSegmentManually;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since
